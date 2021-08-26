ce08818fe921d0bfcc67b6cd12d5d4b2ccc86b88
Tib3rius <48113936+Tib3rius@users.noreply.github.com>
2021-08-26 01:06:08 -0400

    Update nfs.py
    Added mountd plugin.

--- plugins/nfs.py
+++ plugins/nfs.py
@@ -25,3 +25,16 @@ class Showmount(ServiceScan):
 
 	async def run(self, service):
 		await service.execute('showmount -e {address} 2>&1', outfile='{protocol}_{port}_showmount.txt')
+
+class NmapMountd(ServiceScan):
+
+	def __init__(self):
+		super().__init__()
+		self.name = "Nmap Mountd"
+		self.tags = ['default', 'safe', 'nfs']
+
+	def configure(self):
+		self.match_service_name('^mountd')
+
+	async def run(self, service):
+		await service.execute('nmap {nmap_extra} -sV -p {port} --script="banner,nfs* and not (brute or broadcast or dos or external or fuzzer)" -oN "{scandir}/{protocol}_{port}_mountd_nmap.txt" -oX "{scandir}/xml/{protocol}_{port}_mountd_nmap.xml" {address}')
