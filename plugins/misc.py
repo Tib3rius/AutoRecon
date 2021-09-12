3eb890b1aad82a29177e322a755e582a63bcddc9
sordidlist <sordidlistsec@gmail.com>
2021-09-12 01:52:22 -0600

    move irc plugin to misc

--- plugins/misc.py
+++ plugins/misc.py
@@ -66,6 +66,19 @@ class NmapIMAP(ServiceScan):
 	async def run(self, service):
 		await service.execute('nmap {nmap_extra} -sV -p {port} --script="banner,(imap* or ssl*) and not (brute or broadcast or dos or external or fuzzer)" -oN "{scandir}/{protocol}_{port}_imap_nmap.txt" -oX "{scandir}/xml/{protocol}_{port}_imap_nmap.xml" {address}')
 
+class NmapIrc(ServiceScan):
+
+	def __init__(self):
+		super().__init__()
+		self.name = 'Nmap IRC'
+		self.tags = ['default', 'safe', 'irc']
+
+	def configure(self):
+		self.match_service_name('^irc')
+
+	async def run(self, service):
+		await service.execute('nmap {nmap_extra} -sV --script irc-botnet-channels,irc-info,irc-unrealircd-backdoor -oN "{scandir}/{protocol}_{port}_irc_nmap.txt" -oX "{scandir}/xml/{protocol}_{port}_irc_nmap.xml" -p {port} {address}')
+
 class NmapNNTP(ServiceScan):
 
 	def __init__(self):
